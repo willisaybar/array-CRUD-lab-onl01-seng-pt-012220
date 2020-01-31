dd93a22d98288553d726abd0c55d8d97090c94bd
Willis Aybar <willisaybar@gmail.com>
2020-01-31 02:43:49 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,35 +1,38 @@
 def create_an_empty_array
-  
+  [ ]
 end
 
 def create_an_array
-  
+  ["apples", "oranges", "bananas", "grapefruit"]
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  ["array"] << "arrays!"
 end
 
 def add_element_to_start_of_array(array, element)
-  
+  ["array"].unshift("wow")
 end
 
 def remove_element_from_end_of_array(array)
-  
+  ["arrays!"].pop
 end
 
 def remove_element_from_start_of_array(array)
-  
+  ["wow"].shift
 end
 
 def retrieve_element_from_index(array, index_number)
-  
+  array = ["arrays!", "am", "hello"]
+  array(-2)
 end
 
 def retrieve_first_element_from_array(array)
-  
+  array = ["wow", "am", "horse"]
+  array.first
 end
 
 def retrieve_last_element_from_array(array)
-  
+  array = ["wow", "am", "arrays!"]
+  array.last
 end
